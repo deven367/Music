c74d7e82086cecfe3534f236f98487359139d879
kevalvc <chavda.keval97@gmail.com>
2019-06-15 21:03:36 +0530

    Browse Directory Button Function

--- app/src/main/java/com/example/devenmistry/music/MainActivity.java
+++ app/src/main/java/com/example/devenmistry/music/MainActivity.java
@@ -1,42 +1,113 @@
 package com.example.devenmistry.music;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.media.MediaPlayer;
 import android.media.PlaybackParams;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity
 {
-    MediaPlayer mysong;
+    MediaPlayer mysong, myPlayer;
+    SharedPreferences sharedPrefs;
+    SharedPreferences.Editor prefsEditor;
     SeekBar seek;
     Handler h;
     Runnable rr;
+    String custURL = "";
     ImageView playIcon;
+    ImageButton folder;
     AudioTrack audio;
     ImageButton ib;
+    Uri audiouri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mysong = MediaPlayer.create(MainActivity.this,R.raw.audio);
-        mysong.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        if (custURL != "") {
+            audiouri = Uri.parse(custURL);
+            Log.i("mediayolo3", custURL);
+        }
 
 
+        sharedPrefs = getSharedPreferences("PREFS", MODE_PRIVATE);
+        custURL = sharedPrefs.getString("song", "");
+
+//        mysong = MediaPlayer.create(MainActivity.this,R.raw.audio);
+//        mysong.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+//        myPlayer = new MediaPlayer();
+
+        if (audiouri != null) {
+            myPlayer = MediaPlayer.create(MainActivity.this, audiouri);
+            myPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        }
+
+        playIcon = findViewById(R.id.playIcon);
+        folder = findViewById(R.id.folder);
+        folder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.setType("audio/*");
+                startActivityForResult(i, 1);
+
+                Log.i("mediayolo2", custURL);
+
+//                myPlayer.prepareAsync();
+            }
+        });
+
+
+
+//        myPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+//            @Override
+//            public void onPrepared(MediaPlayer playerM){
+//                playerM.start();
+//            }
+//        });
+
+
+/*
+        myPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                myPlayer.start();
+            }
+        });
+
+        myPlayer.prepareAsync();
+*/
+
+//        audiouri = Uri.parse("android.resource://"+getPackageName()+"/raw/audio.mp3");
 
         h = new Handler();
         seek = (SeekBar)findViewById(R.id.seekBar);
 
-        seek.setMax(mysong.getDuration());
+/*
+        seek.setMax(myPlayer.getDuration());
+
+        myPlayer.seekTo(0);
+*/
 
 
         seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -44,7 +115,7 @@ public class MainActivity extends AppCompatActivity
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if(fromUser)
                 {
-                    mysong.seekTo(progress);
+//                    myPlayer.seekTo(progress);
                 }
             }
 
@@ -60,34 +131,49 @@ public class MainActivity extends AppCompatActivity
         });
 
 
-
-
-
-
-    }
-    public void ppz(View v){
-        playIcon = findViewById(R.id.playIcon);
         playIcon.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                if(mysong.isPlaying())
-                {
-                    mysong.pause();
-                    playIcon.setImageResource(R.drawable.ic_play_arrow_black_24dp);
-                } else {
-                    mysong.start();
-                    playIcon.setImageResource(R.drawable.ic_pause_black_24dp);
+            public void onClick(View view) {
+                Log.i("Null", audiouri.toString());
+//                seek.refreshDrawableState();
+                if (audiouri != null) {
+                    myPlayer = MediaPlayer.create(MainActivity.this, audiouri);
+                    myPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    myPlayer.start();
+                    playCycle();
+                    ppz();
                 }
 
             }
         });
+
+
+
+
+    }
+    public void ppz(){
+//        playIcon = findViewById(R.id.playIcon);
+//        playIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+
+                if(myPlayer.isPlaying())
+                {
+                    myPlayer.pause();
+                    playIcon.setImageResource(R.drawable.ic_play_arrow_black_24dp);
+                } else {
+                    myPlayer.start();
+                    playIcon.setImageResource(R.drawable.ic_pause_black_24dp);
+                }
+
+//            }
+//        });
     }
     public void playCycle()
     {
-        seek.setProgress(mysong.getCurrentPosition());
+        seek.setProgress(myPlayer.getCurrentPosition());
 
-        if (mysong.isPlaying())
+        if (myPlayer.isPlaying())
         {
             rr = new Runnable(){
                 @Override
@@ -98,14 +184,15 @@ public class MainActivity extends AppCompatActivity
             h.postDelayed(rr,1000);
         }
     }
+
     public void playIT(View v)
     {
-        if(mysong.isPlaying())
+        if(myPlayer.isPlaying())
         {
-            mysong.pause();
+            myPlayer.pause();
         } else {
-//            playCycle();
-            mysong.start();
+            playCycle();
+            myPlayer.start();
         }
 
     }
@@ -113,25 +200,30 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onPause() {
         super.onPause();
-        mysong.pause();
+//        myPlayer.pause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mysong.start();
+//        myPlayer.start();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mysong.release();
-        h.removeCallbacks(rr);
+//        myPlayer.release();
+//        h.removeCallbacks(rr);
     }
 
     public void ppause(View view)
     {
-        mysong.pause();
+        myPlayer.pause();
+    }
+
+    public void rewind30(View view)
+    {
+
     }
 
     public void previousTrack(View view)
@@ -139,17 +231,29 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    public void folderview(View v) {
-    ib = findViewById(R.id.folder);
-    ib.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+//    Folder button on Main Screen
+    public void folderview( ) {
 
-            Intent intent = getIntent();
-            
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+            if ((data != null) && (data.getData() != null)) {
+
+                audiouri = data.getData(); //declared above Uri audio;
+                Log.i("mediayolo", "onActivityResult: " + audiouri);
+
+                custURL = audiouri.toString();
+
+                prefsEditor = sharedPrefs.edit();
+                prefsEditor.putString("song", custURL);
+                prefsEditor.apply();
+
+            }
         }
-    });
-}
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
 }
